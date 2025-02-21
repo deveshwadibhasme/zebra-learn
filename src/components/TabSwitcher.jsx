98cbd5a6034922f1b7f75a3d982222dc0464badd
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-02-21 22:40:20 +0530

    fix/tabSwitch

--- src/components/TabSwitcher.jsx
+++ src/components/TabSwitcher.jsx
@@ -50,34 +50,40 @@ const TabSwitcher = () => {
 
   return (
     <>
-      <Skeleton width={"w-full"} height={"h-full"} borderRadius={"rounded-lg"} />
-      &&
-      <motion.div
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        className="max-w-6xl w-full h-20 flex gap-4 mx-auto py-2 overflow-x-auto px-2"
-      >
-        <Link
-          to={"/"}
-          className={`w-10 h-10 shrink-0 rounded-full text-white border-1 
+      {(
+        <Skeleton
+          width={"w-full"}
+          height={"h-full"}
+          borderRadius={"rounded-lg"}
+        />
+      ) && (
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          className="max-w-6xl w-full h-20 flex gap-4 mx-auto py-2 overflow-x-auto px-2"
+        >
+          <Link
+            to={"/"}
+            className={`w-10 h-10 shrink-0 rounded-full text-white border-1 
           ${pathname === "/" ? "border-white" : "border-white/15"} 
           flex-center`}
-        >
-          <FontAwesomeIcon icon={faHome} />
-        </Link>
-        {tabs.map((tab, index) => (
-          <Link
-            to={tab.link}
-            key={index}
-            className={`h-10 gap-2 px-4 py-3 rounded-lg text-white/50 hover:text-white cursor-pointer border-1  
+          >
+            <FontAwesomeIcon icon={faHome} />
+          </Link>
+          {tabs.map((tab, index) => (
+            <Link
+              to={tab.link}
+              key={index}
+              className={`h-10 gap-2 px-4 py-3 rounded-lg text-white/50 hover:text-white cursor-pointer border-1  
           ${pathname === `${tab.link}` ? "border-white" : "border-white/15"} 
           flex-center`}
-          >
-            <FontAwesomeIcon icon={tab.icon} />
-            <span className="text-sm font-normal">{tab.label}</span>
-          </Link>
-        ))}
-      </motion.div>
+            >
+              <FontAwesomeIcon icon={tab.icon} />
+              <span className="text-sm font-normal">{tab.label}</span>
+            </Link>
+          ))}
+        </motion.div>
+      )}
     </>
   );
 };
