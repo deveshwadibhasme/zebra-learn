f8d82f0e2db9792874ea86cdb5dc2fc6a4d608e5
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-02-21 17:08:05 +0530

    add/Motion

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,11 +3,16 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { motion } from "framer-motion";
 
-const Carousel = ({slides}) => {
-
+const Carousel = ({ slides }) => {
   return (
-    <div className="w-full max-w-6xl mx-auto p-4 min-h-50 flex-center rounded-lg">
+    <motion.div
+      initial={{ y: 30 , opacity:0.2}}
+      animate={{ y: 0 , opacity:1}}
+      transition= {{duration:1}}
+      className="w-full max-w-6xl mx-auto p-4 min-h-50 flex-center rounded-lg"
+    >
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={30}
@@ -17,17 +22,18 @@ const Carousel = ({slides}) => {
         autoplay={{ delay: 4000, disableOnInteraction: false }}
         className="rounded-lg shadow-lg"
       >
-        {slides.length > 0 && slides.map((src, index) => (
-          <SwiperSlide key={index}>
-            <img
-              src={src}
-              alt={`Slide ${index + 1}`}
-              className="w-full h-full rounded-lg"
-            />
-          </SwiperSlide>
-        ))}
+        {slides.length > 0 &&
+          slides.map((src, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={src}
+                alt={`Slide ${index + 1}`}
+                className="w-full h-full rounded-lg"
+              />
+            </SwiperSlide>
+          ))}
       </Swiper>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/TabSwitcher.jsx
+++ src/components/TabSwitcher.jsx
@@ -10,6 +10,7 @@ import {
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const TabSwitcher = () => {
   const tabs = [
@@ -48,38 +49,36 @@ const TabSwitcher = () => {
   const { pathname } = useLocation();
 
   return (
-    <div className="max-w-6xl w-full h-20 flex gap-4 mx-auto py-2 overflow-x-auto px-2">
-      <Link
-        to={"/"}
-        className={`w-10 h-10 shrink-0 rounded-full text-white border-1 
+    <>
+      <Skeleton width={"w-full"} height={"h-full"} borderRadius={"rounded-lg"} />
+      &&
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        className="max-w-6xl w-full h-20 flex gap-4 mx-auto py-2 overflow-x-auto px-2"
+      >
+        <Link
+          to={"/"}
+          className={`w-10 h-10 shrink-0 rounded-full text-white border-1 
           ${pathname === "/" ? "border-white" : "border-white/15"} 
           flex-center`}
-      >
-        <FontAwesomeIcon icon={faHome} />
-      </Link>
-      {tabs.map((tab, index) => (
-        <>
-          {(
-            <Skeleton
-              width={"w-20"}
-              height={"h-10"}
-              borderRadius={"rounded-lg"}
-            />
-          ) && (
-            <Link
-              to={tab.link}
-              key={index}
-              className={`h-10 gap-2 px-4 py-3 rounded-lg text-white/50 hover:text-white cursor-pointer border-1 
+        >
+          <FontAwesomeIcon icon={faHome} />
+        </Link>
+        {tabs.map((tab, index) => (
+          <Link
+            to={tab.link}
+            key={index}
+            className={`h-10 gap-2 px-4 py-3 rounded-lg text-white/50 hover:text-white cursor-pointer border-1  
           ${pathname === `${tab.link}` ? "border-white" : "border-white/15"} 
           flex-center`}
-            >
-              <FontAwesomeIcon icon={tab.icon} />
-              <span className="text-sm font-normal">{tab.label}</span>
-            </Link>
-          )}
-        </>
-      ))}
-    </div>
+          >
+            <FontAwesomeIcon icon={tab.icon} />
+            <span className="text-sm font-normal">{tab.label}</span>
+          </Link>
+        ))}
+      </motion.div>
+    </>
   );
 };
 
